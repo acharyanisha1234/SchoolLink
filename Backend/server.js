cf9fd3a26fd42020c44bcdd01f3485795148ed68
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-08-06 20:53:30 +0545

    add .env, CORS, JSON middleware and make backend folder structure

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("SchoolLink Backend is running...");
+app.get('/', (req, res) => {
+  res.send('SchoolLink Backend is running...');
 });
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
