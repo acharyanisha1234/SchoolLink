45cd6fe010a873071dec58a15397a9836aa0778e
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-08-19 12:35:54 +0545

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
+    'https://stsschoollink.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true
